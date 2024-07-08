42066ca3b51699478aa5a88275baf8464e22b4b7
Shakhboz <shfazliddinov1940@gmail.com>
2024-07-08 11:42:05 +0500

    ьу

--- app/assets/javascripts/initial_config.js
+++ app/assets/javascripts/initial_config.js
@@ -4,20 +4,4 @@ $(document).on('turbo:load', function() {
   });
   $(function(){$(".datepicker").datepicker({dateFormat: 'dd-mm-yy'})});
   $('#searchInput').focus()
-  document.getElementById('searchInput').addEventListener('input', function() {
-    var input, filter, table, tr, nameAttr, i;
-    input = this.value.toUpperCase();
-    table = document.getElementById('buyers-list');
-    tr = table.getElementsByTagName('tr');
-    for (i = 0; i < tr.length; i++) {
-      nameAttr = tr[i].getAttribute('data-buyer-name');
-      if (nameAttr) {
-        if (nameAttr.toUpperCase().startsWith(input)) {
-          tr[i].style.display = '';
-        } else {
-          tr[i].style.display = 'none';
-        }
-      }
-    }
-  });
 });
